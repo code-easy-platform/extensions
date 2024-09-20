3c21f61d567a4b59cac5ee3eb3cf9509fdf3d049
lvsouza <ls18017@gmail.com>
2024-09-20 15:43:06 -0300

    fix: fix build

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,4 +1,4 @@
-class i {
+class n {
   constructor(e) {
     this._target = e, this.onMessage = null, this._target.addEventListener("message", this._onMessageEvent.bind(this));
   }
@@ -10,7 +10,7 @@ class i {
     e instanceof MessageEvent && e.data.type === "request" && ((t = this.onMessage) == null || t.call(this, e.data.payload), this._respond(e.data.messageId));
   }
 }
-class n {
+class i {
   constructor(e) {
     this._target = e, this._messageQueue = {}, this._target.onerror = this._onErrorEvent.bind(this), this._target.onmessageerror = this._onErrorMessageEvent.bind(this), this._target.addEventListener("error", this._onErrorEvent.bind(this)), this._target.addEventListener("message", this._onMessageEvent.bind(this)), this._target.addEventListener("messageerror", this._onErrorMessageEvent.bind(this));
   }
@@ -44,7 +44,13 @@ class a {
     this._url = e, this._worker = null, this._workerMessageSender = null, this._workerMessageReceiver = null, this.hasError = !1, this.hasInitialized = !1;
   }
   activate() {
-    this._worker = new Worker(this._url, { type: "module" }), this._workerMessageSender = new n(this._worker), this._workerMessageReceiver = new i(this._worker), this._workerMessageReceiver.onMessage = this._onEvent.bind(this), this._workerMessageSender.send({ type: "activate" }).then(() => this.hasInitialized = !0).catch(() => console.warn("Failed to activate a extension"));
+    const e = URL.createObjectURL(
+      new Blob(
+        [`import("${this._url}").then(m => new m.default(self))`],
+        { type: "text/javascript" }
+      )
+    );
+    setTimeout(() => URL.revokeObjectURL(e), 0), this._worker = new Worker(e, { type: "module" }), this._workerMessageSender = new i(this._worker), this._workerMessageReceiver = new n(this._worker), this._workerMessageReceiver.onMessage = this._onEvent.bind(this), this._workerMessageSender.send({ type: "activate" }).then(() => this.hasInitialized = !0).catch(() => console.warn("Failed to activate a extension"));
   }
   deactivate() {
     var e;
@@ -94,7 +100,7 @@ class a {
 }
 class d {
   constructor(e) {
-    this._extensionId = crypto.randomUUID(), this._commands = {}, this._workerReceiver = new i(e), this._workerSender = new n(e), this._commands.activate = this.activate.bind(this), this._commands.deactivate = this.deactivate.bind(this), this._workerReceiver.onMessage = this._onEvent.bind(this);
+    this._extensionId = crypto.randomUUID(), this._commands = {}, this._workerReceiver = new n(e), this._workerSender = new i(e), this._commands.activate = this.activate.bind(this), this._commands.deactivate = this.deactivate.bind(this), this._workerReceiver.onMessage = this._onEvent.bind(this);
   }
   activate() {
   }
